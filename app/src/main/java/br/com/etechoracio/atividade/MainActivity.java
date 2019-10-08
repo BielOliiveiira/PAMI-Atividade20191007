88f78e0dd759b022e56a0b546d18025aa653ac48
BielOliiveiira <gabriel_vm@live.com>
2019-10-07 21:44:14 -0300

    Exercicio 2

--- app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.etechoracio.atividade;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity implements CustomDialog.ItemListener{
@@ -34,4 +35,9 @@ public class MainActivity extends AppCompatActivity implements CustomDialog.Item
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
